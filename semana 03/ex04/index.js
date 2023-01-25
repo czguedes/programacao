8bd481279a95ba0c99194f8e0707fbee4ac05fde
Caio Guedes <116326440+czguedes@users.noreply.github.com>
2023-01-25 00:26:29 -0300

    adiciona aulas

--- semana 03/ex04/index.js
+++ semana 03/ex04/index.js
@@ -69,7 +69,24 @@ console.log('-------------FIM------------------')
 4. Escreva um algoritmo que percorra os números inteiros entre 1 e 250
 e verifique quais números são múltiplos de 3 e quais são múltiplos
 de 5.
+
+---
+contar de 1 a 250
+mostrar números multiplos de 3 e 5
+
 */
+
+let contador = 1
+let multTres
+let multCinco
+
+while (contador <= 250) {
+    if (contador % 3 == 0) {
+        
+    }
+    contador++
+}
+
 console.log('-------------FIM------------------')
 /*
 5. Faça um programa usando a estrutura “for” que leia um número
---
+++ semana 03/index.js
@@ -0,0 +1,29 @@
+//while => laço de repetição com teste lógico no início
+
+/*
+
+while (condition) {
+    
+}
+
+*/
+
+//do while => laço de repetição com teste lógico no fim; garante que o código seja executado pelo menos uma vez
+/*
+
+do {
+    
+} while (condition);
+
+*/
+
+
+//for => ideal para contadores; possui início e fim; cria um escopo
+
+/*
+for (let index = 0; index < 15; index++) {
+    console.log(index);
+    
+}
+
+*/
