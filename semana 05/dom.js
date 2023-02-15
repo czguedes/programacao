8e43e5ee5a5908952e979b7709c06d34325daa06
Caio Guedes <116326440+czguedes@users.noreply.github.com>
2023-02-14 23:25:49 -0300

    inicia estudos dom

---
+++ semana 05/dom.js
@@ -0,0 +1,41 @@
+let lampadaQuebrada = false
+
+function acenderLampada(){
+    if(!lampadaQuebrada){
+        const lampada = document.getElementById('lampada')
+        lampada.src = './imagens/lampada_acesa.jpg'
+    }
+}
+
+function apagarLampada(){
+    if (!lampadaQuebrada) {
+        const lampada = document.getElementById('lampada')
+        lampada.src='./imagens/lampada_apagada.jpg'
+    }
+}
+
+function quebraLampada(){
+    if (!lampadaQuebrada) {
+        
+        const lampada = document.getElementById('lampada')
+        lampada.src = './imagens/lampada_quebrada.jpg'
+        lampadaQuebrada = true
+    
+        //1. cria elemento
+        const botaoTrocar = document.createElement('button') //cria um elemento <button>
+        botaoTrocar.innerText = 'Trocar a lâmpada!' //adiciona texto no <button>
+        botaoTrocar.onclick = trocarLampada //adiciona ação ao clicar
+    
+        //2. adiciona elemento
+        const divContainer = document.querySelector('.container') //seleciona o div.container como local para adicionar o botão
+        divContainer.appendChild(botaoTrocar) //insere botão
+    }
+}
+
+function trocarLampada(){
+    lampadaQuebrada = false
+    apagarLampada()
+
+    const retiraBotao = document.querySelector('button') //seleciona o botão
+    retiraBotao.remove() //remove o botão
+}
