954576fef13f3909a77dd734c7e4c78f3aa3771f
Caio Guedes <116326440+czguedes@users.noreply.github.com>
2023-02-28 19:16:34 -0300

    comenta ex15

--- semana06/ex15/ex15.js
+++ semana06/ex15/ex15.js
@@ -1,37 +1,36 @@
-function recebeForm(event){
-    event.preventDefault()
+function recebeForm(event){ //recebe o input do form
+    event.preventDefault() //previne funções indesejadas
     
-    const form = document.getElementById('formulario')
+    const form = document.getElementById('formulario') //captura o formulário e facilita a sua navegação pelo DOM
 
-    const checkboxes = document.querySelectorAll('input[name="areaInteresse"]:checked')
+    const checkboxes = document.querySelectorAll('input[name="areaInteresse"]:checked') //captura os itens checados no <input type = "checkbox" name="areaInteresse">
 
-    form.nomeUsuario.value ? form.nomeUsuario.value : alert('Digite um nome válido.')
+    form.nomeUsuario.value ? form.nomeUsuario.value : alert('Digite um nome válido.') //validações de campos obrigatórios preenchidos
     form.endUsuario.value ? form.endUsuario.value : alert('Digite um endereço válido.')
     form.cidadeUsuario.value ? form.cidadeUsuario.value : alert('Digite um nome válido.')
     form.estadoUsuario.value ? form.estadoUsuario.value : alert('Escolha um Estado.')
-    if(checkbox.length === 0){
+    if(checkboxes.length === 0){
         alert('Selecione pelo menos uma área de interesse')
         return
-    }
+    } //valida a escolha de um Estado e impede a continuação do logaritmo caso algum campo não tenha sido preenchido 
 
-    const areaInteresse = []
+    const areaInteresse = [] //recebe os itens capturados pela const checkboxes (linha 6)
 
-    for (const item of checkboxes) {
+    for (const item of checkboxes) {    //percorre toda a const checkboxes (linha 6) e dá push nos itens checados
         areaInteresse.push(item.value)
     }
     
-    const usuario = {
+    const usuario = { //cadastra o usuário para cada valor recebido
         nome: form.nomeUsuario.value,
         endereco: form.endUsuario.value,
         cidade: form.cidadeUsuario.value,
         estado: form.estadoUsuario.value,
         natCargo: form.naturezaCargo.value,
-        areaInt: checkboxes,
+        areaInt: areaInteresse, //se fosse usado areaInt: checkboxes seria retornado um array do DOM que dificultaria a leitura e obtenção do valor, por isso da criação e uso da const areaInteresse e do for...of
         bio: form.curriculo.value
     }
         
     alert('Formulário enviado com sucesso!')
     console.log(usuario);
-    form.reset()
+    form.reset() //reseta os campos do formulário após finalizá-lo
 }
-
